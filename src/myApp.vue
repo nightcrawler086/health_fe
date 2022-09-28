b04c0ae1e0d01f0ab49874ab0ac7f8bece2fb6e1
nightcrawler086 <brian.hall4@gmail.com>
2022-09-27 23:33:07 -0400

    this is a button that counts with typescript

---
+++ src/myApp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import TheButton from './components/TheButton.vue'
+</script>
+
+<template>
+  <header>
+    <p>This is the header</p>
+  </header>
+
+  <TheButton />
+</template>
